1e51cd17c2e373e84b790c8168deb3a39266b633
Eliott Morcillo <eliott.morcillo@gmail.com>
2022-05-25 11:35:30 +0200

    feat: removed build folder

--- app/build/static/js/18.170378a2.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpmaterials=this.webpackJsonpmaterials||[]).push([[18],{1882:function(s,t,a){"use strict";a.r(t),t.default=a.p+"static/media/icon.3c32890b.png"}}]);
-//# sourceMappingURL=18.170378a2.chunk.js.map
