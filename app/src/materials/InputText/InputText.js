23f620a39426b48b39208be87bd6332fb7b3f1f5
Eliott Morcillo <eliott.morcillo@gmail.com>
2022-06-23 23:37:41 +0200

    fix: scroll behavior

--- app/src/materials/InputText/InputText.js
+++ app/src/materials/InputText/InputText.js
@@ -1,4 +1,45 @@
 import "./InputText.scss";
+import { alpha, styled } from "@mui/material/styles";
+import { InputBase } from "@mui/material";
+const BootstrapInput = styled(InputBase)(({ theme }) => ({
+  "label + &": {
+    marginTop: theme.spacing(3),
+  },
+  "& .MuiInputBase-input": {
+    margin: "4px",
+    borderRadius: 4,
+    position: "relative",
+    backgroundColor: "var(--card-background)",
+    color: "var(--font-color)",
+    border: "1px solid #ced4da",
+    fontSize: 16,
+    display: "flex",
+    flexGrow: 1,
+    padding: "16px 12px",
+    transition: theme.transitions.create([
+      "border-color",
+      "background-color",
+      "box-shadow",
+    ]),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+    "&:focus": {
+      boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+      borderColor: theme.palette.primary.main,
+    },
+  },
+}));
 const InputText = ({
   value,
   className,
@@ -36,13 +77,12 @@ const InputText = ({
   return (
     <div className="input__text__container">
       <p className="input__text__label">{label}</p>
-      <input
+      <BootstrapInput
         onClick={(e) => {
           e.stopPropagation();
         }}
         type={type}
         disabled={disabled}
-        className={disabled ? "input__text disabled__input" : "input__text"}
         value={value}
         onChange={onChange}
         placeholder={placeHolder}
